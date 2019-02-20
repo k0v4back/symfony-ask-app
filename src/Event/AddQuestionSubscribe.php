0bf7447065ae1bb7b04b9fefe49163c2be9dd87f
Vadim Kosolapov <vadkos33@outlook.com>
2019-02-20 18:22:12 +0200

    Added question subscribe on event

---
+++ src/Event/AddQuestionSubscribe.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Event;
+
+use App\Entity\Notification;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class AddQuestionSubscribe implements EventSubscriberInterface
+{
+    private $entityManager;
+    private $currentUser;
+    private $userRepository;
+
+    public function __construct(
+        EntityManagerInterface $entityManager,
+        AuthenticationUtils $currentUser,
+        UserRepository $userRepository
+    )
+    {
+        $this->entityManager = $entityManager;
+        $this->currentUser = $currentUser;
+        $this->userRepository = $userRepository;
+    }
+    public static function getSubscribedEvents()
+    {
+        return [
+            AddQuestionEvent::NAME => 'addQuestion'
+        ];
+    }
+
+    public function addQuestion(AddQuestionEvent $event)
+    {
+        $user = $event->getUser();
+
+        $userId = $this->userRepository->findOneBy([
+            'email' => $this->currentUser->getLastUsername()
+        ]);
+
+        $notification = new Notification();
+        $notification->setUser($user);
+        $notification->setSeen(false);
+        $notification->setText('Пользователь '. $userId->getNick() .' задал вам вопрос');
+        $notification->setTime(time());
+        $notification->setCreator($userId);
+
+        $this->entityManager->persist($notification);
+        $this->entityManager->flush();
+    }
+
+}
