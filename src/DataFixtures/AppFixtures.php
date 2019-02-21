fdd3844e68260468ad7f9181c898a5c100ff29f8
Vadim Kosolapov <vadkos33@outlook.com>
2019-02-21 18:15:51 +0200

    Fix porblem inside migrations

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -11,55 +11,7 @@ class AppFixtures extends Fixture
 {
     private const USERS = [
         [
-            'name' => 'Test1 test1',
-            'nick' => 'Test1',
-            'email' => 'test@1.com',
-            'password' => '111111',
-            'sex' => 'male',
-            'age' => '10',
-            'country' => 'Russia',
-            'city' => 'Moscow',
-            'avatar' => 'default.jpg',
-            'role' => User::ROLE_USER
-        ],
-        [
-            'name' => 'Test2 test2',
-            'nick' => 'Test2',
-            'email' => 'test@2.com',
-            'password' => '111111',
-            'sex' => 'male',
-            'age' => '31',
-            'country' => 'Russia',
-            'city' => 'Moscow',
-            'avatar' => 'default.jpg',
-            'role' => User::ROLE_USER
-        ],
-        [
-            'name' => 'Test3 test3',
-            'nick' => 'Test3',
-            'email' => 'test@3.com',
-            'password' => 'test3',
-            'sex' => 'woman',
-            'age' => '52',
-            'country' => 'Америка',
-            'city' => 'Moscow',
-            'avatar' => 'default.jpg',
-            'role' => User::ROLE_USER
-        ],
-        [
-            'name' => 'Test1 test1',
-            'nick' => 'Test4',
-            'email' => 'test@1.com',
-            'password' => 'test1',
-            'sex' => 'woman',
-            'age' => '50',
-            'country' => 'Украина',
-            'city' => 'Киев',
-            'avatar' => 'default.jpg',
-            'role' => User::ROLE_USER
-        ],
-        [
-            'name' => 'Косолапов Вадим',
+            'full_name' => 'Косолапов Вадим',
             'nick' => 'k0v4',
             'email' => 'vadkos33@outlook.com',
             'password' => '111111',
@@ -68,6 +20,7 @@ class AppFixtures extends Fixture
             'country' => 'Russia',
             'city' => 'Kaliningrad',
             'avatar' => 'default.jpg',
+            'confirm_token' => 12121212,
             'role' => User::ROLE_ADMIN
         ]
 
@@ -95,7 +48,7 @@ class AppFixtures extends Fixture
         foreach (self::USERS as $userData){
             $user = new User();
 
-            $user->setFullName($userData['name']);
+            $user->setFullName($userData['full_name']);
             $user->setNick($userData['nick']);
             $user->setEmail($userData['email']);
             $user->setPassword($this->encoder->encodePassword($user, $userData['password']));
@@ -103,6 +56,7 @@ class AppFixtures extends Fixture
             $user->setAge($userData['age']);
             $user->setCountry($userData['country']);
             $user->setCity($userData['city']);
+            $user->setConfirmToken($userData['confirm_token']);
             $user->setRoles($userData['role']);
 
             $manager->persist($user);
