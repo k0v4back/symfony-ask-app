7d37eb0c28314e30eeaea087c1959efe1df5d33e
Vadim Kosolapov <vadkos33@outlook.com>
2019-02-22 22:15:21 +0200

    Added crud for user

--- src/Admin/UserAdmin.php
+++ src/Admin/UserAdmin.php
@@ -6,22 +6,32 @@ use Sonata\AdminBundle\Admin\AbstractAdmin;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Form\FormMapper;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class UserAdmin extends AbstractAdmin
 {
     protected function configureFormFields(FormMapper $formMapper)
     {
-        $formMapper->add('userName', TextType::class);
+        $formMapper
+            ->add('fullName', TextType::class)
+            ->add('nick', TextType::class)
+            ->add('email', EmailType::class)
+            ->add('password', PasswordType::class);
     }
 
     protected function configureDatagridFilters(DatagridMapper $datagridMapper)
     {
-        $datagridMapper->add('userName');
+        $datagridMapper->add('fullName')
+            ->add('nick')
+            ->add('email');
     }
 
     protected function configureListFields(ListMapper $listMapper)
     {
-        $listMapper->addIdentifier('userName');
+        $listMapper->addIdentifier('fullName');
+        $listMapper->addIdentifier('nick');
+        $listMapper->addIdentifier('email');
     }
 }
